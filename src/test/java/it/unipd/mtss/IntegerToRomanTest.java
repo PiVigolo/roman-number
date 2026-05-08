9f704b40d9923972564002fbda52b9392f280524
PiVigolo <pietrovigolo.pv@gmail.com>
2026-05-08 10:03:49 +0200

    #5, fatto 7 test per la classe IntegerToRoman

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -12,7 +12,38 @@ public class IntegerToRomanTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testNumberUnderRange() {
+    public void testConvertZeroThrowsException() {
+        // Act
         IntegerToRoman.convert(0);
+    }
+
+    @Test
+    public void testConvertFourteen() {
+        int input = 14;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("XIV", result);
+    }
+    @Test
+    public void testConvertFourHundredNinetyFour() {
+        int input = 494;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("CDXCIV", result);
+    }
+    @Test
+    public void testConvertNineHundredNinetyNine() {
+        int input = 999;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("CMXCIX", result);
+    }
+    @Test
+    public void testConvertThousand() {
+        int input = 1000;
+        String result = IntegerToRoman.convert(input);
+        assertEquals("M", result);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertMoreThousandThrowsException() {
+        // Act
+        IntegerToRoman.convert(1100);
     }
 }
