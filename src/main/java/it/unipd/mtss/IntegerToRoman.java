d6e4057ece32a3df64f2ac1520370ada24c39527
PiVigolo <pietrovigolo.pv@gmail.com>
2026-05-08 09:24:16 +0200

    #4, implementazione prima parte 1-50 con test stampa. Closes #16

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,7 +6,30 @@
 package it.unipd.mtss;
 public class IntegerToRoman {
   public static String convert(int number){
-  // TODO
-  return null;
+    if (number < 1 || number > 1000) {
+        throw new IllegalArgumentException("Numero fuori range");
+    }
+    String result = "";
+    if(number>=50){
+      result+="L";
+      number-=50;
+    }
+    if(number>=10){
+      result+="X";
+      number-=10;
+    }
+    if (number >= 5) {
+        result += "V";
+        number -= 5;
+    }
+    if (number == 4) {
+        result += "IV";
+        number -= 4;
+    }
+    while (number >= 1) {
+        result += "I";
+        number -= 1;
+    }
+    return result;
   }
 }
